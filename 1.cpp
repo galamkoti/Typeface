31cf84c2054359eb126b09ac2b2e50bc8cbf818f
galamkoti <61527615+galamkoti@users.noreply.github.com>
2022-11-21 15:42:14 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,32 @@
-#include<iostream>
+// C++ program to convert decimal
+// number to ternary number
+
+#include <cstdio>
+#include <iostream>
+#include <math.h>
 using namespace std;
-int mian()
+void convertToTernary(int Number)
 {
-  cout<<"Hello";
+	if (Number == 0)
+		return;
+	int x = Number % 3;
+	Number /= 3;
+	if (x < 0)
+		Number += 1;
+	convertToTernary(Number);
+	if (x < 0)
+		cout << x + (3 * -1);
+	else
+		cout << x;
+}
+int main()
+{
+	int a;
+	cin>>a;
+    if (a != 0) {
+		convertToTernary(a);
+	}
+	else
+		cout << "0" << endl;
+// 	return 0;
 }
